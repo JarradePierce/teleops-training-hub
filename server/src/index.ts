c7c1334d22db54b225de947e6963a504800f1bb0
Jarrade Pierce <jarradepierce@yahoo.com>
2026-08-20 12:57:55 -0700

    created schema and migration and built out a little bit of the trainings page

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,4 +1,5 @@
 import "dotenv/config";
+import { prisma } from "./db.js"
 import cors from "cors";
 import express from "express";
 
@@ -9,9 +10,151 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/api/health", (_request, response) => {
-  response.json({ status: "ok" });
+  const healthData = {
+  status: "Healthy",
+  uptime: process.uptime(),
+  timestamp: new Date().toISOString()
+  }
+  response.json(healthData)
 });
+
+// all users
+app.get("/api/users", async (req, res) => {
+  const users = await prisma.user.findMany({
+    select: {
+      id: true,
+      username: true,
+      role: true,
+      audits: true,
+      trainings: true,
+    },
+  })
+  res.json(users)
+})
+
+// user show endpoint 
+app.get("/api/users/:id", async (req, res) => {
+  const userId = Number(req.params.id)
+  const user = await prisma.user.findUnique({
+    where: {
+      id: userId,
+    },
+
+    select: {
+      id: true,
+      username: true,
+      role: true,
+      audits: true,
+      trainings: true,
+    },
+  })
+  if(!user){
+    return res.status(404).json({error: "user not found"})
+  }
+  res.json(user)
+})
+
+// All trainings endpoint
+app.get("/api/trainings", async (req, res) => {
+  const trainings = await prisma.training.findMany({
+    select: {
+      id: true,
+      title: true,
+      description: true,
+      feedback: true,
+      audits: true,
+      links: true,
+      videoLinks: true,
+      users: true,
+    },
+  })
+  res.json(trainings)
+})
+
+// training show endpoint
+app.get("/api/trainings/:id", async (req, res) => {
+  const trainingId = Number(req.params.id)
+  const training = await prisma.training.findUnique({
+    where: {
+      id: trainingId,
+    },
+
+    select: {
+      id: true,
+      title: true,
+      description: true,
+      feedback: true,
+      audits: true,
+      links: true,
+      videoLinks: true,
+      users: true,
+    },
+  })
+  if(!training){
+    return res.status(404).json("error: No training found")
+  }
+  res.json(training)
+})
+
+// all audits
+app.get("/api/audits", async (req, res) => {
+  const audits = await prisma.audit.findMany({
+    select: {
+      id: true,
+      referenceNumber: true,
+      geofence: true,
+      riderService: true,
+      priorityLevel: true,
+      event: true,
+      feedback: true,
+      engagementType: true,
+      trainings: true,
+      userId: true,
+        user: {
+          select: {
+            id: true,
+            username: true,
+          }
+        },
+    },
+  })
+  res.json(audits)
+})
+
+// audit show
+app.get("/api/audits/:id", async (req, res) => {
+  const auditId = Number(req.params.id)
+  const audit = await prisma.audit.findUnique({
+    where: {
+      id: auditId,
+    },
+
+    select: {
+      id: true,
+      referenceNumber: true,
+      geofence: true,
+      riderService: true,
+      priorityLevel: true,
+      event: true,
+      feedback: true,
+      engagementType: true,
+      trainings: true,
+      userId: true,
+        user: {
+          select: {
+            id: true,
+            username: true,
+          },
+        },
+    },
+  })
+  if(!audit){
+    return res.status(404).json("No audit found")
+  }
+  res.json(audit)
+})
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
+
--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,43 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import { browserRouter, route, routes } from "react-router-dom"
 import './App.css'
 
+
+type Training = {
+  id: number,
+  title: string,
+  description: string,
+  feedback: string,
+  audits: TrainingAudit[],
+  links: TrainingLink[],
+  videoLinks: TrainingVideoLink[]
+  users: UserTraining[]
+}
+
 function App() {
-  
+  const [trainings, setTrainings] = useState<Training[]>([])
+
+  useEffect(() => {
+    async function loadTrainings(){
+      const response = await fetch("http://localhost:3000/api/trainings")
+
+      const data = (await response.json()) as Training[]
+      setTraining(data)
+      console.log(data)
+    }
+    loadTrainings()
+  })
+
   return (
     <h1>TeleOps Training Hub</h1>
+
+    <section>
+      {trainings.map((training) => (
+        <h1>title: {training.title} </h1>
+        <h2>description: {training.description} </h2>
+        <p>feedback: {training.feedback} </p>
+      ))}
+    </section>
   )
 }
 
